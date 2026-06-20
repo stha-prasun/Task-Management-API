e51d1c436d93ed495abb1499961c2c20a32f5dba
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-06-20 20:04:44 +0545

    Home Page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Home
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">Welcome To Home</h1>
+        <p className="mt-2">Please login</p>
+        <button
+          onClick={() => navigate("/login")}
+          className="btn btn-primary mt-4"
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
